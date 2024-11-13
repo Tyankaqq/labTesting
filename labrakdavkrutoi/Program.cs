30353c9611bad04809e58727a3cb0be4e3a0a4f0
Your Name <lev.fedorov87@gmail.com>
2024-11-13 18:22:47 +0200

    Добавьте файлы проекта.

---
+++ labrakdavkrutoi/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Random rand = new Random();
+        int arraySize = 10; 
+        int[] numbers = new int[arraySize];
+
+        
+        for (int i = 0; i < arraySize; i++)
+        {
+            numbers[i] = rand.Next(1, 101); 
+        }
+
+        
+        Console.WriteLine("Исходный массив:");
+        PrintArray(numbers);
+
+        
+        Array.Sort(numbers, (x, y) => y.CompareTo(x)); 
+
+       
+        Console.WriteLine("\nОтсортированный массив по убыванию:");
+        PrintArray(numbers);
+    }
+
+    
+    static void PrintArray(int[] array)
+    {
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
